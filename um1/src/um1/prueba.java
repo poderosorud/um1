2eaf9ff5186cf8254f35ae7051f9a2a230b57275
RUBEN <RUBEN@DESKTOP-1AOS2E2>
2022-09-12 08:11:55 -0500

    Version de prueba

---
+++ um1/src/um1/prueba.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package um1;
+
+/**
+ * @author RUBEN
+ *
+ */
+public class prueba {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hola mundo");
+	}
+
+}
